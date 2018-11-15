7dceed8c2302289867514264b9714af1a57df5c6
amingoli78 <amin.foli78@gmail.com>
2018-11-15 20:00:52 +0330

    edit listener by on click

--- app/src/main/java/com/ermile/a05_dialog_listview/MainActivity.java
+++ app/src/main/java/com/ermile/a05_dialog_listview/MainActivity.java
@@ -3,6 +3,7 @@ package com.ermile.a05_dialog_listview;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,12 +15,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView ( R.layout.activity_main );
 
         dialog=findViewById ( R.id.btn_dialog );
-        listview=findViewById ( R.id.btn_listview );
+        dialog.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                startActivity ( new Intent ( MainActivity.this,Dialog.class ) );
+            }
+        } );
 
-        if (dialog.getId () == R.id.btn_dialog){
-            startActivity ( new Intent ( this,Dialog.class ) ); }
-        if (listview.getId ()==R.id.btn_listview){
-            startActivity ( new Intent ( this , ListView.class ) );
-        }
+        listview=findViewById ( R.id.btn_listview );
+        listview.setOnClickListener ( new View.OnClickListener ( ) {
+            @Override
+            public void onClick(View v) {
+                startActivity ( new Intent ( MainActivity.this , ListView.class ) );
+            }
+        } );
+
+
     }
 }
